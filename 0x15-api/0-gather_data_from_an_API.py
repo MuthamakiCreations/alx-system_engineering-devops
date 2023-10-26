35e5edc0c7d53565fe2302b476348355c9765ee2
muthamakicreations <muthamakicreations@gmail.com>
2023-10-26 08:05:56 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,79 @@
 #!/usr/bin/python3
-
-"""
-Gather data from an API
-for a given employee ID, returns information about his/her
-todo list progress.
-"""
-
-
+""" module doc """
 import requests
 import sys
 
 
+def main():
+    """ def com """
+    id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done = f'{todos}&completed=true'
+    notDone = f'{todos}&completed=false'
+    userData = requests.get(f'{url}{users}').json()
+    Name = userData[0].get("name")
+    todosData = requests.get(f'{url}{todos}').json()
+    todosDone = requests.get(f'{url}{done}').json()
+    doneN = len(todosDone)
+    totalN = len(todosData)
+    print(f'Employee {Name} is done with tasks({doneN}/{totalN}):')
+    for task in todosDone:
+        print("\t "+task.get("title"))
+
+
 if __name__ == "__main__":
-    """Gather data from an API"""
+    main()
 
-    # define the base url of rest api
-    url = "https://jsonplaceholder.typicode.com"
+"""
 
-    # send get request to api to get user info in dict format
-    user = requests.get(url + "/users/" + sys.argv[1]).json()
+Employee *NAME* is done with tasks(*DONE*/*TOTAL*):
+     *TITLE*
+     *TITLE*
+     *TITLE*
 
-    # send get request to api to get todo info in dict format
-    todo = requests.get(url + "/todos?userId=" + sys.argv[1]).json()
 
-    # get the number of completed tasks
-    completed_tasks = [task for task in todo if task.get("completed") is True]
+https://jsonplaceholder.typicode.com/users?id=1
+{
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
+https://jsonplaceholder.typicode.com/todos?userId=5
+[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  ]
 
-    # print the result, employee's name, completed tasks & total no of tasks
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed_tasks), len(todo)))
-
-    # print the title of completed tasks
-    for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
+https://jsonplaceholder.typicode.com/todos?userId=5&completed=true
+https://jsonplaceholder.typicode.com/todos?userId=5&completed=false
+  """
